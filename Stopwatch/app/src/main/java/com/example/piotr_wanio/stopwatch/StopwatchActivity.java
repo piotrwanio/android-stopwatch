f9b0639248678ed4055c24d683c1fd7ed35d9ea7
piotrwanio <34741162+piotrwanio@users.noreply.github.com>
2018-02-22 23:26:16 +0100

    added project

---
+++ Stopwatch/app/src/main/java/com/example/piotr_wanio/stopwatch/StopwatchActivity.java
@@ -0,0 +1,30 @@
+package com.example.piotr_wanio.stopwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StopwatchActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+}
